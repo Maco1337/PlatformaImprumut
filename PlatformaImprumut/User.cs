ada56c23283a7b44ddbd0f2a35ecb7a272f05904
georgiana <munteanugeorgiana2005@gmail.com>
2026-01-16 03:30:15 +0200

    Update User.cs

--- PlatformaImprumut/User.cs
+++ PlatformaImprumut/User.cs
@@ -2,21 +2,18 @@
 
 namespace PlatformaImprumut
 {
-    
     public abstract class User
     {
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
     }
-
     
     public class Admin : User 
     { 
         public Admin() => Role = "Admin"; 
     }
 
-    
     public class RegularUser : User 
     { 
         public RegularUser() => Role = "User"; 
