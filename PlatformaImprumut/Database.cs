a00cc7f51d7198e9972ece230bce143892f7ded8
georgiana <munteanugeorgiana2005@gmail.com>
2026-01-16 04:28:33 +0200

    Update Database.cs

--- PlatformaImprumut/Database.cs
+++ PlatformaImprumut/Database.cs
@@ -15,25 +15,37 @@ namespace PlatformaImprumut
         "Carti", 
         "Jocuri"};
 
+        //calea fisierului unde se salveaza datele aplicatiei
         private static readonly string FilePath = "date_platforma.json";
 
+        //salvam datele in fisierul JSON
         public void Save()
         {
             try {
+                //obict->text
                 string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+                //scriem JSON-ul in fisier
                 File.WriteAllText(FilePath, json);
-            } catch (Exception ex) { Console.WriteLine("Eroare salvare: " + ex.Message); }
+            } 
+            // afisam mesaj in caz de eroare
+            catch (Exception ex) 
+            { 
+                Console.WriteLine("Eroare salvare: " + ex.Message); 
+            }
         }
 
         public static Database Load()
         {
+            //ptr eroare returnam o baza de date goala
             if (!File.Exists(FilePath)) return new Database();
             try {
                 string json = File.ReadAllText(FilePath);
+                //text->obiect
                 return JsonSerializer.Deserialize<Database>(json) ?? new Database();
             } 
             catch 
             { 
+                //ptr eroare pornim cu o baza de date goala
                 return new Database(); 
             }
         }
